6a91d9d474328a7689a5bcdfb5557453b325bab2
Gilbert Pellegrom <gilbert@pellegrom.me>
2018-03-07 19:50:34 +0000

    Update footer

--- themes/default/partials/footer.blade.php
+++ themes/default/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                Copyright &copy; {{ date('Y') }} - Circulate is a <a href="https://dev7studios.co" target="_blank">Dev7studios</a> project
+                Copyright &copy; {{ date('Y') }} - Circulate is a project by <a href="https://dev7studios.co" target="_blank">Dev7studios</a>
             </div>
             <div class="col-sm-6">
                 <ul class="nav justify-content-center justify-content-md-end">
@@ -10,10 +10,7 @@
                         <a class="nav-link py-0" href="/docs">Docs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-0" href="/changelog">Changelog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link py-0" href="#">GitHub</a>
+                        <a class="nav-link py-0" href="https://github.com/CirculateCMS/circulate">GitHub</a>
                     </li>
                 </ul>
             </div>
